cf1fc7f9d405fb31f896790537083be22b9d3e9a
Alan Liu <alanliu@exosite.com>
2020-01-17 16:42:51 +0800

    add log

--- services/interface/setIdentityState.lua
+++ services/interface/setIdentityState.lua
@@ -1,3 +1,6 @@
 local result = Device2.getIdentity(operation)
-log.debug('setIdentityState overload: ' .. to_json(result))
+log.debug('setIdentityState overload: ' .. to_json({
+    operation = operation,
+    result = result
+}))
 return result
