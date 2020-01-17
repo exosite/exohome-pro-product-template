bbec5bc8bb214ea0818b7ca9405d5be641c66c5a
Alan Liu <alanliu@exosite.com>
2020-01-17 17:08:45 +0800

    finish setIdentityState overload

--- modules/interfaceOverload.lua
+++ modules/interfaceOverload.lua
@@ -1,12 +1,18 @@
 local interfaceOverload = {}
 
 function interfaceOverload.setIdentityState(operation)
-    local result = Device2.setIdentityState(operation)
-    log.debug('setIdentityState overload: ' .. to_json({
-        operation = operation,
-        result = result
-    }))
-    return result
+    if operation.data_out ~= nil then
+        local data_out = from_json(operation.data_out)
+        if data_out ~= nil then
+            local action = {
+                data = data_out,
+                id = 'exosense',
+                request = 'set'
+            }
+            operation.action = to_json(action)
+        end
+    end
+    return Device2.setIdentityState(operation)
 end
 
 return interfaceOverload
