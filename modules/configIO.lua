147f066d4c822c1a1d225a504e4cbcf0e2dea007
Calvin Chu <calvinchu8172@gmail.com>
2021-05-19 16:57:24 +0800

    Add config_io control flow: 1. If under provision condition 2. If config_io is changed then send new config_io.

--- modules/configIO.lua
+++ modules/configIO.lua
@@ -1,10 +1,10 @@
-local R = require('moses')
+local M = require('moses')
 local configIO = {}
 
 function configIO.convertFields(fields)
     local reported = from_json(fields.reported)
 
-    local channels = R(reported)
+    local channels = M(reported)
       :map(function(x)
         if R.isString(x) then
           return {x}
@@ -33,7 +33,7 @@ function configIO.convertFields(fields)
       end, {})
       :value()
 
-    channels = configIO.combineOtherChannels(channels)
+    channels = configIO.combineResourcesChannels(channels)
 
     local config_io = to_json({ channels = channels })
 
@@ -44,97 +44,143 @@ function configIO.convertFields(fields)
     }
 end
 
-function configIO.combineOtherChannels(channels)
+function configIO.combineResourcesChannels(channels)
 
-    local protocol_config = {
-      report_rate = 30000,
-      timeout = 30000
-    }
-
-    local properties_number = {
-      control = true,
-      data_type = "NUMBER",
-      locked = true,
-      primitive_type = "NUMERIC"
-    }
-
-    local properties_string = {
-      control = true,
-      data_type = "STRING",
-      locked = true,
-      primitive_type = "STRING"
-    }
-
-    local properties_location = {
-      data_type = "LOCATION",
-      data_unit = "LAT_LONG",
-      locked = true,
-      primitive_type = "JSON"
-    }
-
-    local other_channels = {
-      brand = {
-        display_name = "brand",
-        protocol_config = protocol_config,
-        properties = properties_string
-      },
-      class = {
-        display_name = "class",
-        protocol_config = protocol_config,
-        properties = properties_number
-      },
-      esh_version = {
-        display_name = "esh_version",
-        protocol_config = protocol_config,
-        properties = properties_string
-      },
-      device_id = {
-        display_name = "device_id",
-        protocol_config = protocol_config,
-        properties = properties_string
-      },
-      firmware_version = {
-        display_name = "firmware_version",
-        protocol_config = protocol_config,
-        properties = properties_string
-      },
-      local_ip = {
-        display_name = "local_ip",
-        protocol_config = protocol_config,
-        properties = properties_string
-      },
-      location = {
-        display_name = "location",
-        protocol_config = protocol_config,
-        properties = properties_location
-      },
-      mac_address = {
-        display_name = "mac_address",
-        protocol_config = protocol_config,
-        properties = properties_string
-      },
-      model = {
-        display_name = "model",
-        protocol_config = protocol_config,
-        properties = properties_string
-      },
-      provisioned_time = {
-        display_name = "provisioned_time",
-        protocol_config = protocol_config,
-        properties = properties_number
-      },
-      ssid = {
-        display_name = "ssid",
-        protocol_config = protocol_config,
-        properties = properties_string
-      }
-    }
-
-  local config_io = R.extend(channels, other_channels)
+  local resources_channels = configIO.resourcesChannels()
+  local config_io = M.extend(channels, resources_channels)
 
   return config_io
 
 end
 
+function configIO.checkCurrentResourcesChannels()
+  local current_resources_channels = Keystore.get({ key = "resources_channels" }).value
+  local new_resources_channels = configIO.resourcesChannels()
+  local resources_channels_changed
+  if current_resources_channels == nil or not M.isEqual(from_json(current_resources_channels), new_resources_channels) then
+    Keystore.set({ key = "resources_channels", value = to_json(new_resources_channels) })
+    resources_channels_changed = true
+  else
+    resources_channels_changed = false
+  end
+  return resources_channels_changed
+end
+
+function configIO.resourcesChannels()
+
+  local protocol_config = {
+    report_rate = 30000,
+    timeout = 30000
+  }
+
+  local properties_number = {
+    control = true,
+    data_type = "NUMBER",
+    locked = true,
+    primitive_type = "NUMERIC"
+  }
+
+  local properties_string = {
+    control = true,
+    data_type = "STRING",
+    locked = true,
+    primitive_type = "STRING"
+  }
+
+  local properties_location = {
+    data_type = "LOCATION",
+    data_unit = "LAT_LONG",
+    locked = true,
+    primitive_type = "JSON"
+  }
+
+  local resources_channels = {
+    brand = {
+      display_name = "brand",
+      protocol_config = protocol_config,
+      properties = properties_string
+    },
+    class = {
+      display_name = "class",
+      protocol_config = protocol_config,
+      properties = properties_number
+    },
+    esh_version = {
+      display_name = "esh_version",
+      protocol_config = protocol_config,
+      properties = properties_string
+    },
+    device_id = {
+      display_name = "device_id",
+      protocol_config = protocol_config,
+      properties = properties_string
+    },
+    firmware_version = {
+      display_name = "firmware_version",
+      protocol_config = protocol_config,
+      properties = properties_string
+    },
+    local_ip = {
+      display_name = "local_ip",
+      protocol_config = protocol_config,
+      properties = properties_string
+    },
+    location = {
+      display_name = "location",
+      protocol_config = protocol_config,
+      properties = properties_location
+    },
+    mac_address = {
+      display_name = "mac_address",
+      protocol_config = protocol_config,
+      properties = properties_string
+    },
+    model = {
+      display_name = "model",
+      protocol_config = protocol_config,
+      properties = properties_string
+    },
+    provisioned_time = {
+      display_name = "provisioned_time",
+      protocol_config = protocol_config,
+      properties = properties_number
+    },
+    ssid = {
+      display_name = "ssid",
+      protocol_config = protocol_config,
+      properties = properties_string
+    }
+  }
+
+  return resources_channels
+end
+
+function configIO.checkOrInitiateCounter(event, resources)
+  local set_config_io_counter
+  if resources.set_config_io_counter == nil then
+    set_config_io_counter = 0
+    Device2.setIdentityState({
+      identity = event.identity,
+      set_config_io_counter = 0
+    })
+  else
+    set_config_io_counter = resources.set_config_io_counter.set
+  end
+  return set_config_io_counter
+end
+
+function configIO.resetCounter(event)
+  Device2.setIdentityState({
+    identity = event.identity,
+    set_config_io_counter = 1
+  })
+end
+
+function configIO.incrementCounter(event, set_config_io_counter)
+  Device2.setIdentityState({
+    identity = event.identity,
+    set_config_io_counter = set_config_io_counter + 1
+  })
+end
 
 return configIO
--- services/device2/event.lua
+++ services/device2/event.lua
@@ -8,45 +8,64 @@ local M = require('moses')
 log.debug('BEFORE:' .. to_json(event))
 local resources = Device2.getIdentityState({
   identity = event.identity
-  })
-
-local location = ipToGeo.convertIP(event.ip)
-
-local esh, module_data, provisioned_time = {}
-local fields
-
-if resources.provisioned_time ~= nil then
-  esh = from_json(resources.esh.reported)
-  module_data = from_json(resources.module.reported)
-  provisioned_time = { provisioned_time = resources.provisioned_time.set }
-  fields = resources.fields.reported
-end
+})
 
 if event.payload ~= nil then
   for idx, pl in ipairs(event.payload) do
-    -- copy 'states' and merge with resources data then transmit to 'data_in' and 'data_out' (for notify UI ack)
+    -- Copy 'states' and merge with resources data then transmit to 'data_in' and 'data_out' (for notify UI ack)
     if pl.values['states'] ~= nil then
+
       local states = from_json(pl.values['states'])
+      local esh, module_data, provisioned_time = {}
+      local fields
+      if resources.provisioned_time ~= nil then
+        esh = from_json(resources.esh.reported)
+        module_data = from_json(resources.module.reported)
+        provisioned_time = { provisioned_time = resources.provisioned_time.set }
+        fields = resources.fields.reported
+      end
+
+      local location = ipToGeo.convertIP(event.ip)
       local resources_data = to_json(M.extend(states, esh, module_data, provisioned_time, location ))
-      log.debug(resources_data)
+
       pl.values['data_in'] = resources_data
       pl.values['data_out'] = resources_data
+
+      -- Fake config_io data-in to update exosense config_io cache when detected config_io changed
+      if fields ~= nil then
+
+        local set_config_io_counter
+        set_config_io_counter = configIO.checkOrInitiateCounter(event, resources)
+
+        local state = {
+          set = fields,
+          reported = fields,
+          timestamp = pl.timestamp
+        }
+        local config_io = configIO.convertFields(state).reported
+
+        local config_io_changed = configIO.checkCurrentResourcesChannels()
+        if config_io_changed == true then
+          log.debug('Set new config_io')
+          pl.values['config_io'] = config_io
+          configIO.resetCounter(event)
+        elseif set_config_io_counter <= 2 then -- Only allow sending config_io twice after set config_io
+          pl.values['config_io'] = config_io
+          configIO.incrementCounter(event, set_config_io_counter)
+        end
+      end
     end
 
-    -- fake config_io data-in, to update exosense config_io cache
-    if fields ~= nil then
-      local origin_config_io = Keystore.get({ key = "config_io" }).value
+    -- Fake config_io data-in to update exosense config_io cache when provisioned
+    if pl.values['fields'] ~= nil then
+      log.debug('Set config_io when provisioned')
       local state = {
-        set = fields,
-        reported = fields,
+        set = pl.values['fields'],
+        reported = pl.values['fields'],
         timestamp = pl.timestamp
       }
-      local new_config_io = configIO.convertFields(state).reported
-      if origin_config_io == nil or origin_config_io ~= new_config_io then
-        log.debug('Set new config_io')
-        Keystore.set({ key = "config_io", value = new_config_io })
-        pl.values['config_io'] = new_config_io
-      end
+      local config_io = configIO.convertFields(state).reported
+      pl.values['config_io'] = config_io
     end
   end
 end
