bf6bcaf4b943a26f88b598edf580abae9799ca4d
Calvin Chu <calvinchu8172@gmail.com>
2021-05-28 14:57:16 +0800

    Error handling if Geo service has error.

--- modules/ipToGeo.lua
+++ modules/ipToGeo.lua
@@ -4,10 +4,15 @@ function ipToGeo.convertIP(ip)
 	local ret = Geo.ipToGeo({
 		ip = ip
 	})
-	local latitude = ret.value.coordinates[2]
-	local longitude = ret.value.coordinates[1]
-	local location = { location = to_json({ lat = latitude, lng = longitude }) }
-	return location
+	if ret.error ~= nil then
+  		log.debug(to_json(ret))
+  		return nil
+	else
+		local latitude = ret.value.coordinates[2]
+		local longitude = ret.value.coordinates[1]
+		local location = { location = to_json({ lat = latitude, lng = longitude }) }
+		return location
+	end
 end
 
 return ipToGeo
